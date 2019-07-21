db343e2311e492e5edc59e4a37a5af5b76751ccb
El1Leon <Edwar.Gleon@gmail.com>
2019-07-21 00:41:50 -0400

    Update Day3IntroToConditionalStatements.py

--- Day3IntroToConditionalStatements.py
+++ Day3IntroToConditionalStatements.py
@@ -4,7 +4,7 @@ if N % 2 == 1:
     print("Weird")
 elif N % 2 == 0 & 2 <= N <= 5 :
     print("Not Weird")
-elif N % 2 != 0 & 6 <= N <= 20:
+elif N % 2 == 0 & 6 <= N <= 20:
     print("Weird")
 else:
     print("Not Weird")
